4c5212abd8fbad6eb384f687221a23eb90704e3f
surajdabral <80247787+surajdabral@users.noreply.github.com>
2021-11-12 11:49:59 +0530

    Delete node.js

--- node.js
+++
@@ -1,9 +0,0 @@
-const http = require('http');
-
-const server  = http.createServer((req, res) {
-    res.end('Hello, World');
-});
-
-server.listen(10000,"127.0.0.1",(){
-    consolelog("listening to the port 10000")
-});
